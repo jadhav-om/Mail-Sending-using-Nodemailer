519355ed6aaf9d1c8b25f2adb0de7bc413f05414
Your Name <you@example.com>
2025-01-09 10:54:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const nodemailer = require("nodemailer");
+
+const server = http.createServer((request, response) => {
+    const auth = nodemailer.createTransport({
+        service: "gmail",
+        secure : true,
+        port : 465,
+        auth: {
+            user: "jadhavom284@gmail.com",
+            pass: "ufyp fmut zhol kdcq"
+
+        }
+    });
+
+    const receiver = {
+        from : "jadhavom284@gmail.com",
+        to : "omjadhav.rcpit@gmail.com",
+        subject : "Node Js Mail Testing!",
+        text : "Hello this is a text mail!"
+    };
+
+    auth.sendMail(receiver, (error, emailResponse) => {
+        if(error)
+        throw error;
+        console.log("success!");
+        response.end();
+    });
+    
+});
+
+server.listen(8080);
